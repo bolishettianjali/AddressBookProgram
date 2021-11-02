2271a67e9ba5a75b6b5fe7e44fc866dc72038ff9
bolishettianjali <bolishettianjali@gamil.com>
2021-11-03 02:11:50 +0530

    Added To take user option for action using switch case and do while

--- src/com/addressBook/AddressBookProgram.java
+++ src/com/addressBook/AddressBookProgram.java
@@ -3,26 +3,67 @@ import java.util.Scanner;
 
 public class AddressBookProgram {
 
+
     static Scanner scanner = new Scanner(System.in);
     static Contact[] contacts;
+    static int noOfContacts;
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program\n");
+        //This variable is used to exit the do while loop when user enters Exit as option.
+        boolean flag = true;
+        System.out.println("Welcome to Address Book Program");
 
-        //To take no.of contacts to be added for creating an array for saving contact objects.
-        System.out.println("Enter how many Contacts do you want add: ");
-        int noOfContacts = scanner.nextInt();
-        contacts = new Contact[noOfContacts]; //Created an array with size of no.of contacts to be add.
+        //do while is used to continue the Address book system until user enters Exit.
+        do {
+            //This line is to display options.
+            System.out.println(
+                    "\nEnter a Option: \n1. Add Contact\n2. Display All Contacts" + "\n3. Exit");
+            int option = scanner.nextInt();
 
-        //for loop to take details and save Contact objects in Contact array.
-        for (int i = 0; i < contacts.length; i++) {
-            System.out.println("\nEnter person-"+(i+1)+" details: \n");
-            Contact contact = addContact();
-            contacts[i] = contact;
+            switch (option) {
+                case 1:
+                    //This if conditions checks weather Contacts arrys is empty or not,
+                    //If array is empty it creates an Contact array.
+                    if (contacts == null) {
+                        System.out.println("Enter how many Contacts do you want add: ");
+                        noOfContacts = scanner.nextInt();
+                        contacts = new Contact[noOfContacts];
+                        noOfContacts = 0;
+                    }
+                    //It Checks the contacts added in array were less than array size,
+                    // and have space in array, if its true then a new contact will be added.
+                    if (noOfContacts < contacts.length) {
+                        System.out.println("\nEnter person-" + (noOfContacts + 1) + " details: \n");
+                        Contact contact = addContact();
+                        contacts[noOfContacts] = contact;
+                        noOfContacts++;
+                    } else {
+                        System.out.println("\nAddress book is full can not be added new Contact");
+                    }
+                    break;
+                case 2:
+                    displayPersonNames();
+                    break;
+                case 3:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Enter any one of the above option: \n");
+            }
+        } while (flag);
+        System.out.println("------------Thank You--------------");
+    }
+
+    //This method is to print First name in every contact object which are in array.
+    public static void displayPersonNames() {
+        System.out.println("\nAll Contacts: ");
+        for (int i = 0; i < noOfContacts; i++) {
+            System.out.println((i + 1) + ". " + contacts[i].firstName);
         }
     }
 
-    //Method to take input and create a Contact
+    /*This method is to take person details from console and create contact object,
+    and returns Contact object.*/
     public static Contact addContact() {
         System.out.println("Enter First Name:");
         String firstName = scanner.next();
@@ -41,13 +82,9 @@ public class AddressBookProgram {
         System.out.println("Enter email:");
         String email = scanner.next();
 
-        Contact contact = new Contact(firstName, lastName, address, city,
-                state, zip, phoneNumber, email);
-
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
         System.out.println(contact);
-
+        System.out.println("\nContact Added to Address Book successfully.");
         return contact;
     }
-
 }
-
